423a274aed2b393364126153f31534ad75a5925a
Santiago Sastre Guzman <santiago@santiagosastre.com>
2026-05-28 17:46:50 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+#Psammite Virtual Machine
+## A simple 64-bit VM written in C.
+###Specification:
+* Architecture: 64-bit word size, little endian.
+* Instruction width: fixed 32-bit instructions.
+* Memory: 64 KB, byte-addressable.
+* Stack: full-descending (grows downwards, points to the last element).
+* ABI rule: Each function (Callee) must  restore the Link Register and Base Pointer of their caller.
+* Program counter: single 64-bit register (PC).
+* Integer registers (32x64-bit):
+  * R0/ZR is hardwired to 0.
+  * R1/SP: Stack Pointer.
+  * R2/BP: Base Pointer (Anchors the stack frame for local variables).
+  * R3/LR: Link Register (holds the return address).
+  * R4-R31: general purpose registers.
+* Floating-point registers: 32x64-bit registers (FR0-FR31).
---
+++ psammite.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+
+#define NUM_REGISTER 32
+#define VM_MEM_SIZE 65536
+
+
+typedef union {
+  uint64_t bits;
+  double as_float;
+} FRegister;
+
+typedef struct {
+  uint64_t pc;
+  uint64_t registers[NUM_REGISTER];
+  FRegister f_registers[NUM_REGISTER];
+  uint8_t memory[VM_MEM_SIZE];
+} VM;
+
+
+
+int vm_reset(VM *vm) {
+  vm->pc = 0;
+  memset(vm->registers, 0, sizeof(vm->registers));
+  memset(vm->f_registers,0,sizeof(vm->f_registers));
+  memset(vm->memory, 0, sizeof(vm->memory));
+  return 0;
+}
+
+int vm_init(VM *vm) {
+  if (vm_reset(vm)!= 0 ) {
+    return 1;
+  }
+  return 0;
+  
+}
+
+int vm_load_program(VM *vm, uint32_t *program, size_t program_size){
+  if (program_size > VM_MEM_SIZE) {
+    fprintf(stderr, "FATAL ERROR: Program too large for VM memory.");
+    return 1;
+  }
+  memcpy(vm->memory, program, program_size);
+  return 0;
+}
+
+int main() {
+  return 0;
+}
