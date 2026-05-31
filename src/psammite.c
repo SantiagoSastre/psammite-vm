0ecaadafb690dba79d673f7fec68d68823cc5091
Santiago Sastre Guzman <santiago@santiagosastre.com>
2026-05-31 12:55:23 -0600

    Added new, free functions and register enums

--- src/psammite.c
+++ src/psammite.c
@@ -1,20 +1,93 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdint.h>
+#include <stdlib.h>
 
 #define NUM_REGISTER 32
 #define VM_MEM_SIZE 65536
 
 
+typedef enum {
+  ZR = 0,
+  SP = 1,
+  BP = 2,
+  LR = 3,
+  R4 = 4,
+  R5 = 5,
+  R6 = 6,
+  R7 = 7,
+  R8 = 8,
+  R9 = 9,
+  R10 = 10,
+  R11 = 11,
+  R12 = 12,
+  R13 = 13,
+  R14 = 14,
+  R15 = 15,
+  R16 = 16,
+  R17 = 17,
+  R18 = 18,
+  R19 = 19,
+  R20 = 20,
+  R21 = 21,
+  R22 = 22,
+  R23 = 23,
+  R24 = 24,
+  R25 = 25,
+  R26 = 26,
+  R27 = 27,
+  R28 = 28,
+  R29 = 29,
+  R30 = 30,
+  R31 = 31
+} Register;
+
+
+
+typedef enum {
+  FR0 = 0,
+  FR1 = 1,
+  FR2 = 2,
+  FR3 = 3,
+  FR4 = 4,
+  FR5 = 5,
+  FR6 = 6,
+  FR7 = 7,
+  FR8 = 8,
+  FR9 = 9,
+  FR10 = 10,
+  FR11 = 11,
+  FR12 = 12,
+  FR13 = 13,
+  FR14 = 14,
+  FR15 = 15,
+  FR16 = 16,
+  FR17 = 17,
+  FR18 = 18,
+  FR19 = 19,
+  FR20 = 20,
+  FR21 = 21,
+  FR22 = 22,
+  FR23 = 23,
+  FR24 = 24,
+  FR25 = 25,
+  FR26 = 26,
+  FR27 = 27,
+  FR28 = 28,
+  FR29 = 29,
+  FR30 = 30,
+  FR31 = 31
+} FRegister;
+
 typedef union {
   uint64_t bits;
   double as_float;
-} FRegister;
+} PsammiteFloat;
 
 typedef struct {
   uint64_t pc;
   uint64_t registers[NUM_REGISTER];
-  FRegister f_registers[NUM_REGISTER];
+  PsammiteFloat f_registers[NUM_REGISTER];
   uint8_t memory[VM_MEM_SIZE];
 } PsammiteVM;
 
@@ -25,7 +98,9 @@ int psammite_reset(PsammiteVM *vm) {
   memset(vm->registers, 0, sizeof(vm->registers));
   memset(vm->f_registers,0,sizeof(vm->f_registers));
   memset(vm->memory, 0, sizeof(vm->memory));
-
+  
+  //Set the Stack Pointer to the end of memory
+  vm->registers[SP] = VM_MEM_SIZE;
   
   
   return 0;
@@ -39,6 +114,19 @@ int psammite_init(PsammiteVM *vm) {
   
 }
 
+PsammiteVM* psammite_new() {
+  PsammiteVM* vm = malloc(sizeof(PsammiteVM));
+  if (vm == NULL) {
+    return NULL;
+  }
+  psammite_init(vm);
+  return vm;
+}
+
+void psammite_free(PsammiteVM *vm) {
+  free(vm);
+}
+
 int psammite_load_program(PsammiteVM *vm, uint8_t *program, size_t program_size){
   if (program_size > VM_MEM_SIZE) {
     fprintf(stderr, "FATAL ERROR: Program too large for VM memory.");
@@ -49,6 +137,10 @@ int psammite_load_program(PsammiteVM *vm, uint8_t *program, size_t program_size)
 }
 
 
+void psammite_dump(PsammiteVM *vm) {
+  printf("--------Psammite VM--------");
+}
+
 int psammite_run(PsammiteVM *vm) {
   
   return 0;
@@ -56,5 +148,9 @@ int psammite_run(PsammiteVM *vm) {
 
 
 int main() {
+  PsammiteVM* vm = psammite_new();
+  psammite_dump(vm);
+  psammite_free(vm);
+  vm = NULL;
   return 0;
 }
