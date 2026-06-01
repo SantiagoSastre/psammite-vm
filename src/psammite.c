332f1475a503fbfe0650da732246e170c72fb250
Santiago Sastre Guzman <santiago@santiagosastre.com>
2026-05-31 21:23:27 -0600

    added dump function and improved CMakeLists.txt

--- src/psammite.c
+++ src/psammite.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <inttypes.h>
 
 #define NUM_REGISTER 32
 #define VM_MEM_SIZE 65536
@@ -86,6 +87,7 @@ typedef union {
 
 typedef struct {
   uint64_t pc;
+  uint32_t ir;
   uint64_t registers[NUM_REGISTER];
   PsammiteFloat f_registers[NUM_REGISTER];
   uint8_t memory[VM_MEM_SIZE];
@@ -138,17 +140,59 @@ int psammite_load_program(PsammiteVM *vm, uint8_t *program, size_t program_size)
 
 
 void psammite_dump(PsammiteVM *vm) {
-  printf("--------Psammite VM--------");
+  printf("---------------------------Psammite VM------------------------------------------------------------------------------------------\n");
+  printf("IR: 0x%08X              |    PC: 0x%016" PRIX64 "\n",vm->ir, vm->pc);
+  printf("-----------------------Integer Registers----------------------------------------------------------------------------------------\n");
+  for(Register i = ZR; i<NUM_REGISTER; i++) {
+    if (i%4==0 && i!=ZR) {
+      printf("\n");
+    }
+    char reg_name[4];
+    switch (i) {
+      case ZR:
+        strcpy(reg_name, "ZR");
+        break;
+      case SP:
+        strcpy(reg_name, "SP");
+        break;
+      case BP:
+        strcpy(reg_name, "BP");
+        break;
+      case LR:
+        strcpy(reg_name, "LR");
+        break;
+      default:
+        snprintf(reg_name, sizeof(reg_name), "R%02d", i);
+    }
+    printf("%-4s: 0x%016" PRIX64 "    |    ",reg_name , vm->registers[i] );
+  }
+  printf("\n");
+
+  printf("-----------------------Float Registers------------------------------------------------------------------------------------------\n");
+  
+  for(FRegister i = FR0; i<NUM_REGISTER; i++) {
+    if (i%4==0 && i!=FR0) {
+      printf("\n");
+    }
+    char reg_name[5];
+    snprintf(reg_name, sizeof(reg_name), "FR%02d", i);
+    printf("%-4s: %018.6lf    |    ",reg_name , vm->f_registers[i].as_float );
+  }
+  printf("\n");
 }
 
-int psammite_run(PsammiteVM *vm) {
-  
-  return 0;
-}
+//int psammite_run(PsammiteVM *vm) {
+//
+//  return 0;
+//}
 
 
 int main() {
   PsammiteVM* vm = psammite_new();
+  if (vm == NULL) {
+    fprintf(stderr, "VM could not be instantiated.");
+    return 1;
+  }
   psammite_dump(vm);
   psammite_free(vm);
   vm = NULL;
